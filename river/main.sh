637c96070f217f25f0f9a19d2de4cf2811a7e570
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2024-12-29 01:11:31 +0700

    add jl software

--- river/main.sh
+++ river/main.sh
@@ -1,1 +1,7 @@
-# this one is just the poc for ui job without modify the url in reverse proxy
+# pull singularity if it does not exist
+if [ ! -f ~/.river/images/singularities/images/all-spark-notebook_latest.sif ]; then
+    singularity pull --dir <<river_home>>.river/images/singularities/images docker://quay.io/jupyter/all-spark-notebook
+fi
+
+# run jupyter lab
+singularity run --writable --cleanenv <<river_home>>/.river/images/singularities/images/all-spark-notebook_latest.sif jupyter lab --NotebookApp.token='' --port $PORT
