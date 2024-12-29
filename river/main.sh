749045b67df5d3311a6932eece05bd8c282f7725
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2024-12-29 18:19:25 +0700

    fix river home

--- river/main.sh
+++ river/main.sh
@@ -1,6 +1,6 @@
 # pull singularity if it does not exist
 if [ ! -f ~/.river/images/singularities/images/all-spark-notebook_latest.sif ]; then
-    singularity pull --dir <<river_home>>.river/images/singularities/images docker://quay.io/jupyter/all-spark-notebook
+    singularity pull --dir <<river_home>>/.river/images/singularities/images docker://quay.io/jupyter/all-spark-notebook
 fi
 
 # run jupyter lab
