9c1c6f2d68604ce67a7398c2fc68dc1c5b93e106
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-01-01 18:52:32 +0700

    update river home

--- river/main.sh
+++ river/main.sh
@@ -1,7 +1,7 @@
 # pull singularity if it does not exist
-if [ ! -f ~/.river/images/singularities/images/all-spark-notebook_latest.sif ]; then
-    singularity pull --dir <<river_home>>/.river/images/singularities/images docker://quay.io/jupyter/all-spark-notebook
+if [ ! -f $RIVER_HOME/.river/images/singularities/images/all-spark-notebook_lab-4.3.4.sif ]; then
+    singularity pull --dir $RIVER_HOME/.river/images/singularities/images docker://quay.io/jupyter/all-spark-notebook:lab-4.3.4
 fi
 
 # run jupyter lab
-singularity run --writable --cleanenv <<river_home>>/.river/images/singularities/images/all-spark-notebook_latest.sif jupyter lab --NotebookApp.token='' --NotebookApp.base_url='/<<uuid_job_id>>' --port $PORT --NotebookApp.allow_origin='*'
+singularity run --writable --cleanenv $RIVER_HOME/.river/images/singularities/images/all-spark-notebook_lab-4.4.3.sif jupyter lab --NotebookApp.token='' --NotebookApp.base_url='/<<uuid_job_id>>' --port $PORT --NotebookApp.allow_origin='*'
