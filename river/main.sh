427d754f254832fdf565f9a6ff7928fd7423feab
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-09-07 18:21:54 +0700

    feat: add the host to allow reverse proxy in LAN

--- river/main.sh
+++ river/main.sh
@@ -1,3 +1,3 @@
 ENV=jupyterlab-$tag
 micromamba create -n $ENV conda-forge::jupyterlab=4.4.6 -y
-micromamba run -n $ENV jupyter lab --NotebookApp.token='' --port $PORT --NotebookApp.allow_origin='*' --NotebookApp.base_url=$job_id
+micromamba run -n $ENV jupyter lab --ip=$(hostname) --NotebookApp.token='' --port $PORT --NotebookApp.allow_origin='*' --NotebookApp.base_url=$job_id --no-browser
